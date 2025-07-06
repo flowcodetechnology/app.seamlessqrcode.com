fc7515d4f51e7b296ab6f62c330d0b9a58da80bb
flowcodetechnology <support@flowcode.co.ke>
2025-07-06 05:27:47 +0300

    first commit

---
+++ app.seamlessqrcode.com/config.php
@@ -0,0 +1,8 @@
+<?php
+
+/* Configuration of the site */
+define('DATABASE_SERVER',   'localhost');
+define('DATABASE_USERNAME', 'gponzghq_adminseamless2');
+define('DATABASE_PASSWORD', 'Sinister101@1738');
+define('DATABASE_NAME',     'gponzghq_seamlessqrcodeextended');
+define('SITE_URL',          'https://app.seamlessqrcode.com/');
